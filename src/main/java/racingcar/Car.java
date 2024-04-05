4b73362e7be180a9298a1cb69cc8f024a7bb195e
ff1451 <ff1451@gmail.com>
2024-04-05 19:40:18 +0900

    Feat : Car 클래스 생성 및 속성 부여

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Car {
+    private final String carName;
+    private int position;
+
+    public Car(String carName) {
+        this.carName = carName;
+        this.position = 0;
+    }
+}
